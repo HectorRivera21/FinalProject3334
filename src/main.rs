2c8dc7ff1e13a53b8c9c55ab2e54269b9b81d952
Hector <redmagix2000@gmail.com>
2024-05-08 19:06:24 -0500

    sa

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use rayon::prelude::*;
 use std::fs::File;
+#[allow(unused_imports)]use std::env;
 use std::io::{self, BufRead, BufReader};
 use std::collections::HashMap;
+#[allow(unused_imports)]use std::sync::{Arc, Mutex,MutexGuard};
 use std::time::Instant;
 
 fn main() {
     let file_paths = ["Data/data1.txt","Data/data2.txt","Data/data3.txt","Data/data4.txt","Data/data5.txt","Data/data6.txt","Data/data7.txt","Data/dataWhale8.txt","Data/data9.txt","Data/data10.txt"];
-
     println!("Sequential Reading");
     match sequential_reading(&file_paths) {
         Ok(word_count) => println!("Unique words (sequential): {:?}", word_count.len()),
@@ -25,58 +26,35 @@ fn main() {
         Err(e) => eprintln!("Error in pipelined reading: {:?}", e),
     };
 }
-
 fn sequential_reading(file_path:&[&str]) -> io::Result<HashMap<String, i32>> {
     let start = Instant::now();
     let mut word_count:HashMap<String,i32> = HashMap::new();
-    for (i,_value) in file_path.into_iter().enumerate(){
-        let file = File::open(file_path[i])?;
+    for (_i, file_name) in file_path.into_iter().enumerate(){
+
+        let file = File::open(file_name).expect("counld not open file");
         let reader = BufReader::new(file);
 
         for line in reader.lines() {
-            let line = line?; // Handle possible error when reading a line
-            let line_words:Vec<&str> = line.split_ascii_whitespace().collect();
-            for word in line_words {
-                *word_count.entry(word.to_string()).or_insert(0) += 1;
+
+            let line = line.expect("could not read line"); // Handle possible error when reading a line
+            for word in line.split_ascii_whitespace() {
+
+                *word_count.entry(word.to_string().to_lowercase()).or_insert(0) += 1;
+
             }
         }
     }
+    let duration = start.elapsed();
     println!("###################################################################################");
-    println!("Duration: {:?}",start.elapsed());
+    println!("Duration: {:?}",duration);
     Ok(word_count)
 }
-
-fn parallel_reading(file_path:&[&str]) -> io::Result<HashMap<String, i32>> {
-    let mut global_word_count: HashMap<String, i32>  = HashMap::new();        
+fn parallel_reading(file_paths: &[&str]) -> io::Result<HashMap<String, i32>> {
     let start = Instant::now();
-    let word_counts: Result<Vec<_>, _> = file_path.par_iter().map(|&file_name| {
-        count_words(file_name)
-    }).collect();
-
-    let word_counts = word_counts?;
-
-    for word_count in word_counts {
-        for (word, count) in word_count {
-            *global_word_count.entry(word).or_insert(0) += count;
-        }
-    }
+    let mut global_word_count: HashMap<String, i32> = HashMap::new();
+    println!("Parallel processing duration: {:?}", start.elapsed());
     println!("###################################################################################");
-    println!("Duration: {:?}",start.elapsed());
     Ok(global_word_count)
-}
-fn count_words(file_path: &str) -> io::Result<HashMap<String, i32>> {
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
-
-    let mut word_count = HashMap::new();
-    for line in reader.lines() {
-        let line = line?;
-        for word in line.split_whitespace() {
-            *word_count.entry(word.to_string()).or_insert(0) += 1;
-        }
-    }
-
-    Ok(word_count)
 }
 fn pipelined_reading(file_path:&[&str]) -> io::Result<HashMap<String, i32>> {
     let global_word_count: HashMap<String, i32>  = HashMap::new();
@@ -90,4 +68,3 @@ fn pipelined_reading(file_path:&[&str]) -> io::Result<HashMap<String, i32>> {
     println!("Duration:{:?}",start.elapsed());
     Ok(global_word_count)
 }
-
